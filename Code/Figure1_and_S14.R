03cb3796565cdf9ffad4988e01c4fa26ce0f9a97
hp33gika <hpereira@idiv.de>
2024-02-26 21:02:22 +0100

    Changed to blind color pallette

--- Code/Figure1_and_S14.R
+++ Code/Figure1_and_S14.R
@@ -87,8 +87,9 @@ geomchange<-function(deltay,deltatime)
   ((1+deltay/100)^(1/deltatime)-1)*100
 }
 
-b<-c("#56B4E9","#009E73","#D55E00","#FCBF10")
+#b<-c("#56B4E9","#009E73","#D55E00","#FCBF10")
 #b<-c("#56B4E9","#D55E00","#009E73","#FCBF10")
+b<-c("#F2C45F", "#B5520F","#00681C","#56B4E9")
 
 ### 2 - Import data ----
 data_all<-read.csv(file.path(root_tables,"Biodiversity_global.csv"))
